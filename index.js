95e948b45bcea97226576937b1b47575cda853b7
dev-nayandas <rich.nayan@gmail.com>
2022-11-08 01:10:13 +0600

    first api created

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('service review is running')
+})
+
+
+app.listen(port, ()=>{
+    console.log(`service review is running on ${port}`)
+})
