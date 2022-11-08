ecb5ed3c24eff5c2d0f37a3770abc1b16ad34301
dev-nayandas <rich.nayan@gmail.com>
2022-11-09 03:39:54 +0600

    database connectes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 require('dotenv').config();
 
 
@@ -10,9 +12,37 @@ app.use(cors());
 app.use(express.json());
 
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pfvoz.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log(uri)
+async function run(){
+        try{
+            const serviceCollection = client.db('serviceReview').collection('services')
+            app.get('/services', async (req, res)=>{
+                const query = {}
+                const cursor=  serviceCollection.find(query);
+                const services = await cursor.toArray();
+                res.send(services)
+            })
+        }
+        finally{
+
+        }
+}
+run().catch(err => console.log(err));
+
+
+
+
+
+
+
 app.get('/', (req, res) => {
     res.send('service review is running')
 })
+
+
 
 
 app.listen(port, ()=>{
