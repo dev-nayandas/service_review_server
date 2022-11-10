0f13e173a0cefd3b2b3acb72780b1193badba9e6
dev-nayandas <rich.nayan@gmail.com>
2022-11-10 12:06:19 +0600

    review api created

--- index.js
+++ index.js
@@ -19,11 +19,31 @@ console.log(uri)
 async function run(){
         try{
             const serviceCollection = client.db('serviceReview').collection('services')
+            const reviewCollection = client.db('serviceReview').collection('reviews')
+
+
+
             app.get('/services', async (req, res)=>{
                 const query = {}
                 const cursor=  serviceCollection.find(query);
                 const services = await cursor.toArray();
                 res.send(services)
+            });
+
+
+            app.get('/reviews', async (req, res)=>{
+                const query = {}
+                const cursor=  reviewCollection.find(query);
+                const reviews = await cursor.toArray();
+                res.send(reviews)
+            });
+
+            app.get('/reviews/:id', async (req, res)=>{
+                const  id =req.params.id;
+                const query = {_id: ObjectId(id)};
+                const cursor=  reviewCollection.find(query);
+                const review = await cursor.toArray();
+                res.send(review)
             });
 
             app.get('/services/:id', async(req, res)=>{
@@ -38,6 +58,13 @@ async function run(){
             app.post('/services', async (req, res) => {
                 const service = req.body;
                 const result = await serviceCollection.insertOne(service);
+                res.send(result);
+            });
+
+
+            app.post('/reviews', async (req, res) => {
+                const review = req.body;
+                const result = await reviewCollection.insertOne(review);
                 res.send(result);
             });
 
